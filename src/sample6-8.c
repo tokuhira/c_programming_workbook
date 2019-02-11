2c69aad37750d55a3b72e50594d6c6399382eda7
tokuhira <masatsune@tokuhira.net>
2019-02-11 15:31:17 +0900

    日本語入力しといたよ。 sample6-7.cはバグがあったので、日本語以外の差分もチェックしてください。 sample6-8.cは「bille」から「bills」に変数名を変更しているので、差分が多いけど、内容はそのままです。 どちらにせよ、数字を色々入れてみて動作を確認してくださいね。

--- src/sample6-8.c
+++ src/sample6-8.c
@@ -2,57 +2,59 @@
 
 int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
 {/*「main(void){」で始まり「}」で終わります*/
-    /*money:支払額,price:買い物額,change:おつり,bille:紙幣や硬貨の枚数*/
-    int money,price,change,bille;
+    /*money:支払額,price:買い物額,change:おつり,bills:紙幣や硬貨の枚数*/
+    int money,price,change,bills;
     
-    printf("");
+    printf("買い物した金額を入力してください：");
     scanf("%d",&price);
     
-    printf("");
+    printf("支払った金額を入力してください：");
     scanf("%d",&money);
     
-    printf("\n\n\n");
+    printf("\n***** おつり一覧 *****\n\n");
     
-    /*おつり*/
+    /* おつりを計算します */
     change=money-price;
     
-    /*5000*/
-    bille=change/5000;
-    printf("%d\n",bille);
+    /* 5000円札の枚数を求める。int型どうしの割り算なので余りを切り捨てます */
+    bills=change/5000;
+    printf("5000円札 ： %d\n",bills);
+    
+    /* 5000で割った余りを％演算で求めて残額を求めます */
     change=change%5000;
     
-    /*1000*/
-    bille=change/1000;
-    printf("%d\n",bille);
+    /* 1000円札の枚数を求め、残額を計算します */
+    bills=change/1000;
+    printf(1000円札 ： "%d\n",bills);
     change=change%1000;
     
-    /*500*/
-    bille=change/500;
-    printf("%d\n",bille);
+    /* 500円玉の枚数をもとめ、残額を計算します */
+    bills=change/500;
+    printf("500円玉 ： %d\n",bills);
     change=change%500;
     
-    /*100*/
-    bille=change/100;
-    printf("%d\n",bille);
+    /* 100円玉の枚数を求め、残額を計算します */
+    bills=change/100;
+    printf("100円玉 ： %d\n",bills);
     change=change%100;
     
-    /*50*/
-    bille=change/50;
-    printf("%d\n",bille);
+    /* 50円玉の枚数を求め、残額を計算します */
+    bills=change/50;
+    printf("50円玉 ： %d\n",bills);
     change=change%50;
     
-    /*10*/
-    bille=change/10;
-    printf("%d\n",bille);
+    /* 10円玉の枚数を求め、残額を計算します */
+    bills=change/10;
+    printf("10円玉 ： %d\n",bills);
     change=change%10;
     
-    /*5*/
-    bille=change/5;
-    printf("%d\n",bille);
+    /* 5円玉の枚数を求め、残額を計算します */
+    bills=change/5;
+    printf("5円玉 ： %d\n",bills);
     change=change%5;
     
-    /*1*/
-    printf("%d\n",bille);
+    /* 1円玉の枚数を求める */
+    printf("1円玉 ： %d\n",bills);
     
     return 0;/*プログラムを終了する命令です*/
 }/*「main(void){」で始まり「}」で終わります*/
