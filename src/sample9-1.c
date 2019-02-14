d53a2114816b9b0bfcfa4ebf7a4ea51c26b4bd10
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-02-14 14:31:38 +0900

    p.106

---
+++ src/sample9-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int counter;
+    
+    for(counter=0;counter<4;counter=counter+1){
+        printf("****\n");
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
