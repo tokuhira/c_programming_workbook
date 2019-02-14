c7a9f12f4887a339b63bc91e06efab2ab990a598
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-02-14 14:22:56 +0900

    p.98

---
+++ src/sample8-3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int month=0
+    
+    printf("\n");
+    scanf("%d",&month)
+    
+    if(month==1){
+        printf("\n",month);
+    }else if(month==2){
+        printf("\n",month);
+    }else if(month==3){
+        printf("\n",month);
+    }else if(month==4){
+        printf("\n",month);
+    }else if(month==5){
+        printf("\n",month);
+    }else if(month==6){
+        printf("\n",month);
+    }else if(month==7){
+        printf("\n",month);
+    }else if(month==8){
+        printf("\n",month);
+    }else if(month==9){
+        printf("\n",month);
+    }else if(month==10){
+        printf("\n",month);
+    }else if(month==11){
+        printf("\n",month);
+    }else if(month==12){
+        printf("\n",month);
+    }else(month==2){
+        printf("\n",month);
+    }
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
