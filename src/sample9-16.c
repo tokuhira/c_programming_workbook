9bf297ac295989138767a97e5c12e4b7531c38cd
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-03-20 13:51:45 +0900

    p.128_EC

---
+++ src/sample9-16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a[5]={1,2,3,4,5};
+    int counter;/*添え字用の変数*/
+    int sum;/*合計を入れる変数*/
+
+    sum=0;/*sumに初期値0を代入している*/
+    for(counter=0;counter<5;counter++){
+        sum=sum+a[counter];
+        printf("counte=%d a[counter]=%d sum=%d\n",counter,a[counter],sum);
+    }
+    printf("sum=%d\n",sum);
+
+    return 0;
+    
+
+    
+}
