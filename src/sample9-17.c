c45f31f108a7d6df3918a01bee32ab938be9bba3
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-03-20 13:57:10 +0900

    p.128_EC

--- src/sample9-17.c
+++ src/sample9-17.c
@@ -5,13 +5,15 @@ int main(void)
     int a[5]={1,2,3,4,5};
     int counter;/*添え字用の変数*/
     int sum;/*合計を入れる変数*/
+    double mean;/*平均を入れる変数*/
 
     sum=0;/*sumに初期値0を代入している*/
     for(counter=0;counter<5;counter++){
         sum=sum+a[counter];
         printf("counte=%d a[counter]=%d sum=%d\n",counter,a[counter],sum);
     }
-    printf("sum=%d\n",sum);
+    mean=(double)sum/5.0;
+    printf("平均=%f",mean);
 
     return 0;
 }
