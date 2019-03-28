6231d3a0d2f0e3a83516278c6586a68c83d57f0f
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-03-28 17:15:29 +0900

    p.134

---
+++ src/sample10-1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int counter;
+    double kion[12];
+    double kosuiryo[12]=0;
+
+    printf("\n");
+
+    for(counter=0;counter<12;counter++){
+        printf("%2d 月=",counter+1);
+        scanf("%lf %lf",&kion[counter],&kosuiryo[counter]);
+    }
+    printf("月：　　　　平均気温[℃]　　　　降水量[mm]\n");
+
+    for(counter=0;counter<12;counter++){
+    /*月の名前の数字はcounter+1になります */    
+        printf("%2d 月：%10.1f %10.1f\n",counter+1,kion[counter],kosuiryo[counter]);
+    }
+    
+    return 0;
+}
