ceac749236e50fd9e09a0902e3bf63eb3c94d9a3
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-03-30 12:09:56 +0900

    p.150

---
+++ src/sample11-11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int counter;
+    int jogen;
+    int kagen;
+    kagen=10;
+    jogen=20;
+    
+    for(counter=kagen;counter<jogen;counter++){
+        printf("%d\n",counter);
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
