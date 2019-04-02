106b72bb33d5b00db4d63a06d336dc50f9f40ef9
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-04-02 15:57:06 +0900

    p.153_CE

---
+++ src/sample11-14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int index;
+    int data[10]={1,3,5,7,9,11,13,15,17,19};
+    
+    for(index=0;index<10;index++){
+        printf("data[%d] の値は %d\n",index,data[index]);
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
