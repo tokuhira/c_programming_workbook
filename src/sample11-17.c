8f6db53c3fce3eda56b2cc6c33ad0c3608c57805
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-04-04 16:34:08 +0900

    p.156

---
+++ src/sample11-17.c
@@ -0,0 +1,17 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int index;
+    int data[10];
+    /* まず入力 */
+    for(index=0;index<10;index++){
+        printf("data[%d]",index);
+        scanf("%d",&data[index]);
+    }
+    for(index=0;index<10;index++){
+        printf("data[%d] の値は %d\n",index,data[index]);
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
