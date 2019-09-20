3818a9807c23100741b4d299a444c3ff63bccc80
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-09-20 17:54:47 +0900

    Confirmed execution 11-20 correction

--- src/sample11-20.c
+++ src/sample11-20.c
@@ -3,26 +3,19 @@
 int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
 {/*「main(void){」で始まり「}」で終わります*/
     int index;
-    int data[10]={1,3,5,7,9,11,13,15,17,19};
+    int data[10]={1,3,5,5,7,9,9,11,13,15};
     int sagasu;
-    int mitsuketa=0;
-    int basho;
+    int kosuu=0;
     
     printf("探すデータは何ですか？\n");
     scanf("%d",&sagasu);
 
     for(index=0;index<10;index++){
         if(data[index]==sagasu){
-            mitsuketa=1;
-            basho=index;
-            break;
+            kosuu=kosuu+1;
         }
     }
-    if(mitsuketa==1){
-        printf("data[%d]に見つけました\n",basho);
-    }else{
-        printf("見つかりませんでした\n");
-    }
+    printf("%dという値のデータは%d個ありました。\n",sagasu,kosuu);
     
     return 0;/*プログラムを終了する命令です*/
 }/*「main(void){」で始まり「}」で終わります*/
