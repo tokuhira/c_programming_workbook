0bae4c02ae1e41ef231723aefc83545a0186704d
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2019-09-20 17:36:58 +0900

    Confirmed execution sample11-19~20

---
+++ src/sample11-20.c
@@ -0,0 +1,28 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int index;
+    int data[10]={1,3,5,7,9,11,13,15,17,19};
+    int sagasu;
+    int mitsuketa=0;
+    int basho;
+    
+    printf("探すデータは何ですか？\n");
+    scanf("%d",&sagasu);
+
+    for(index=0;index<10;index++){
+        if(data[index]==sagasu){
+            mitsuketa=1;
+            basho=index;
+            break;
+        }
+    }
+    if(mitsuketa==1){
+        printf("data[%d]に見つけました\n",basho);
+    }else{
+        printf("見つかりませんでした\n");
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
