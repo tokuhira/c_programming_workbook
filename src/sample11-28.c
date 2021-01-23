33e6600854386d4397ff98f12be8a618eb92e882
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2021-01-23 15:54:28 +0900

    Create sample11-28.c

---
+++ src/sample11-28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int index;
+    int deta[10] = {1,3,5,7,9,11,13,15,17,19};
+    int sowa = 0;
+    double heikin; /*平均は端数が出るのでdouble型を使用する*/
+
+    for (index = 0; index < 10; index++){
+        sowa = sowa + deta[index];
+    }
+    heikin = (double)sowa / 10;
+    printf("%f\n",heikin);
+
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
