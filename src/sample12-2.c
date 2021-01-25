86bdfe799d4f501783a270716d321f626ae67df0
Shunnosuke Tokuhira <shunnosuke@tokuhira.net>
2021-01-25 10:20:04 +0900

    Create sample12-2.c
    日本語未入力

---
+++ src/sample12-2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>/*あらかじめ用意されているC言語の入出力関係の機能（ライブラリ）を「使いますよ」という宣言をしています。*/
+
+int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
+{/*「main(void){」で始まり「}」で終わります*/
+    int n;
+    int money = 0;
+    int target = 0;
+    double rata = 0.0;
+    double prate;
+    int total = 0;
+
+    printf ("目標金額\n");
+    scanf ("%d", &target);
+
+    printf ("利率は何％ですか？\n");
+    scanf ("%lf",&prate);
+
+    printf ("初期金額\n");
+    scanf ("%d", &money);
+    /* ％で表された利率を割合に変換します */
+    rata = prate / 100;
+    total = money;
+    n = 0;
+    while (1){
+        if (total >= target){
+            break;
+        }
+        total = total * (1 + rata);
+        n++
+    }
+    
+    return 0;/*プログラムを終了する命令です*/
+}/*「main(void){」で始まり「}」で終わります*/
