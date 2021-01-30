c9f3091deb85c8508f036e239610e81cb89eccdd
tokuhira <masatsune@tokuhira.net>
2021-01-30 21:25:08 +0900

    Update sample12-2.c
    日本語入力と、行の入れ替えによる写経度アップと、教科書のミス修正と。

--- src/sample12-2.c
+++ src/sample12-2.c
@@ -3,33 +3,37 @@
 int main(void)/*C言語のプログラムでは「int main(void)」から実行が始まります*/
 {/*「main(void){」で始まり「}」で終わります*/
     int n;
-    int money = 0;
     int target = 0;
-    double rata = 0.0;
+    int money = 0;
+    double rate = 0.0;
     double prate;
     int total = 0;
 
-    printf ("目標金額\n");
+    printf ("あなたの目標金額はいくらですか？\n");
     scanf ("%d", &target);
+
+    printf ("最初にいくら預けますか？\n");
+    scanf ("%d", &money);
 
     printf ("利率は何％ですか？\n");
     scanf ("%lf",&prate);
 
-    printf ("初期金額\n");
-    scanf ("%d", &money);
     /* ％で表された利率を割合に変換します */
-    rata = prate / 100;
+    rate = prate / 100;
     total = money;
     n = 0;
     while (1){
         if (total >= target){
             break;
         }
-        total = total * (1 + rata);
+        total = total * (1 + rate);
         n++;
     }
 
-    printf("合計%d 円になります。\n",total);
+    printf("目標金額：%d 円\n", target);
+    printf("%d 円を%f %%で預けると\n", money, prate);
+    printf("%d 年かかります。\n", n);
+    printf("合計%d 円になります。\n", total);
     
     return 0;/*プログラムを終了する命令です*/
 }/*「main(void){」で始まり「}」で終わります*/
